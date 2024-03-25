3f0c8943ebb13e125c768138a3d534516c9d356b
MUKILTEO\1713357 <Kolbscash@gmail.com>
2024-03-25 10:22:29 -0700

    last batle function push before leaving

--- NeonVoid/Assets/Kaycee/BattleCode.cs
+++ NeonVoid/Assets/Kaycee/BattleCode.cs
@@ -1,4 +1,5 @@
 
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -12,16 +13,18 @@ public class BattleCode : MonoBehaviour
     public Transform[] BattlePoints;
     public GameObject Enemy;
     public GameObject player;
-    public GameObject[] currentDeck;
-    public GameObject[] deckSave;
-    private bool turnActive, turnInactive;
-    public Transform currentPlayerLoc;
-    
+    public GameObject[] currentDeck;// the players current deck, may need to figure out how to set and remove values
+    public GameObject[] deckSave; // The deck saved externally, will be used when reseting players active deck, and during reshuffles as well
+    private bool turnActive, turnInactive, turnReady; //turn values
+    public Transform currentPlayerLoc; // where the player currently is
+    public GameObject[] activeHand; // what the player has in their hand
+    public int RandomValue;// randomizes the order for when cards are drawn
+    public GameObject nextDraw; // for when cards influnce draw order, and we cant just randomize it out 
     #endregion
     // Start is called before the first frame update
     void Start()
     {
-       
+        currentDeck = deckSave;
     }
 
     // Update is called once per frame
@@ -33,6 +36,19 @@ public class BattleCode : MonoBehaviour
             Enemy.GetComponent<EnemyStats>().enemyEffects();
             roundStart();
         }
+        if (Enemy.GetComponent<EnemyStats>().health >= 0)
+        {
+            exitCombat();
+        }
+        if(player.GetComponent<PlayerStats>().health == 0)
+        {
+            //skill issue
+            gameOver();
+        }
+        if(turnReady == true)
+        {
+
+        }
 
         if (turnActive == false)
         {
@@ -42,6 +58,7 @@ public class BattleCode : MonoBehaviour
     }
     public void roundStart()
     {
+        
         turnActive = true;
     }
  
@@ -49,4 +66,13 @@ public class BattleCode : MonoBehaviour
     {
         turnActive = false;
     }
+    public void exitCombat()
+    {
+        // do exit combat function
+    }
+    public void gameOver()
+    {
+        // end game
+    }
+    
 }
