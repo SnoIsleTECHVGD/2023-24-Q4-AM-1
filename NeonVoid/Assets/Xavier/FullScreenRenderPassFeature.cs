d5d1a8a6639444c2a75b430f6118e369fe2eeec4
Xavier-Ford <XavierFord1040@gmail.com>
2024-04-14 20:10:20 -0700

    I don't like the bugs from hunt showdown
    Bugs under your fingernails
    Bugs under your fingernails
    Bugs under your fingernails

--- NeonVoid/Assets/Xavier/FullScreenRenderPassFeature.cs
+++ NeonVoid/Assets/Xavier/FullScreenRenderPassFeature.cs
@@ -13,17 +13,16 @@ public class FullScreenRenderPassFeature : ScriptableRendererFeature
         [SerializeField]
         private Material material;
         RTHandle tempTexture, sourceTexture;
+        private LayerMask noPixelLayerMask;
+        private LayerMask pixelateLayerMask;
 
-        public CustomRenderPass(Material material) : base()
+        public CustomRenderPass(Material material, LayerMask noPixelLayerMask, LayerMask pixelateLayerMask) : base()
         {
             this.material = material;
+            this.noPixelLayerMask = noPixelLayerMask;
+            this.pixelateLayerMask = pixelateLayerMask;
         }
 
-        // This method is called before executing the render pass.
-        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
-        // When empty this render pass will render to the active camera render target.
-        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
-        // The render pipeline will ensure target setup and clearing happens in a performant manner.
         public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
         {
             sourceTexture = renderingData.cameraData.renderer.cameraColorTargetHandle;
@@ -34,10 +33,6 @@ public class FullScreenRenderPassFeature : ScriptableRendererFeature
                     name: "_TempTexture");
         }
 
-        // Here you can implement the rendering logic.
-        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
-        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
-        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
             CommandBuffer commandBuffer = CommandBufferPool.Get("Full Screen Render Feature");
@@ -50,14 +45,28 @@ public class FullScreenRenderPassFeature : ScriptableRendererFeature
             commandBuffer.GetTemporaryRT(
                 Shader.PropertyToID(tempTexture.name), targetDescriptor, FilterMode.Bilinear);
 
-            Blit(commandBuffer, sourceTexture, tempTexture, material);
-            Blit(commandBuffer, tempTexture, sourceTexture);
+            // Check if the current camera's culling mask intersects with the "No Pixel" layer mask
+            if ((renderingData.cameraData.camera.cullingMask & noPixelLayerMask) == 0)
+            {
+                // If the camera's culling mask intersects with the "No Pixel" layer mask, just copy the source texture to the target
+                Blit(commandBuffer, sourceTexture, sourceTexture);
+            }
+            else if ((renderingData.cameraData.camera.cullingMask & pixelateLayerMask) != 0)
+            {
+                // If the camera's culling mask intersects with the "Pixelate" layer mask, apply the material
+                Blit(commandBuffer, sourceTexture, tempTexture, material);
+                Blit(commandBuffer, tempTexture, sourceTexture);
+            }
+            else
+            {
+                // If the camera's culling mask doesn't intersect with either the "No Pixel" or "Pixelate" layer masks, just copy the source texture to the target
+                Blit(commandBuffer, sourceTexture, sourceTexture);
+            }
 
             context.ExecuteCommandBuffer(commandBuffer);
             CommandBufferPool.Release(commandBuffer);
         }
 
-        // Cleanup any allocated resources that were created during the execution of this render pass.
         public override void OnCameraCleanup(CommandBuffer cmd)
         {
             tempTexture.Release();
@@ -66,17 +75,16 @@ public class FullScreenRenderPassFeature : ScriptableRendererFeature
 
     CustomRenderPass m_ScriptablePass;
 
-    /// <inheritdoc/>
     public override void Create()
     {
-        m_ScriptablePass = new CustomRenderPass(this.material);
+        LayerMask noPixelLayerMask = ~(1 << LayerMask.NameToLayer("No Pixel"));
+        LayerMask pixelateLayerMask = 1 << LayerMask.NameToLayer("Pixelate");
+        m_ScriptablePass = new CustomRenderPass(this.material, noPixelLayerMask, pixelateLayerMask);
 
-        // Configures where the render pass should be injected.
+// Before render
         m_ScriptablePass.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
     }
 
-    // Here you can inject one or multiple render passes in the renderer.
-    // This method is called when setting up the renderer once per-camera.
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
     {
         renderer.EnqueuePass(m_ScriptablePass);
