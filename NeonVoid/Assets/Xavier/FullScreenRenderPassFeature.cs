47bfca3209c79db3a438c8e8e0dd72ae6070ac30
Xavier-Ford <XavierFord1040@gmail.com>
2024-04-14 22:18:11 -0700

    I should probably go to bed.
    😴

--- NeonVoid/Assets/Xavier/FullScreenRenderPassFeature.cs
+++ NeonVoid/Assets/Xavier/FullScreenRenderPassFeature.cs
@@ -13,14 +13,10 @@ public class FullScreenRenderPassFeature : ScriptableRendererFeature
         [SerializeField]
         private Material material;
         RTHandle tempTexture, sourceTexture;
-        private LayerMask noPixelLayerMask;
-        private LayerMask pixelateLayerMask;
 
-        public CustomRenderPass(Material material, LayerMask noPixelLayerMask, LayerMask pixelateLayerMask) : base()
+        public CustomRenderPass(Material material) : base()
         {
             this.material = material;
-            this.noPixelLayerMask = noPixelLayerMask;
-            this.pixelateLayerMask = pixelateLayerMask;
         }
 
         public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
@@ -45,23 +41,9 @@ public class FullScreenRenderPassFeature : ScriptableRendererFeature
             commandBuffer.GetTemporaryRT(
                 Shader.PropertyToID(tempTexture.name), targetDescriptor, FilterMode.Bilinear);
 
-            // Check if the current camera's culling mask intersects with the "No Pixel" layer mask
-            if ((renderingData.cameraData.camera.cullingMask & noPixelLayerMask) == 0)
-            {
-                // If the camera's culling mask intersects with the "No Pixel" layer mask, just copy the source texture to the target
-                Blit(commandBuffer, sourceTexture, sourceTexture);
-            }
-            else if ((renderingData.cameraData.camera.cullingMask & pixelateLayerMask) != 0)
-            {
-                // If the camera's culling mask intersects with the "Pixelate" layer mask, apply the material
-                Blit(commandBuffer, sourceTexture, tempTexture, material);
-                Blit(commandBuffer, tempTexture, sourceTexture);
-            }
-            else
-            {
-                // If the camera's culling mask doesn't intersect with either the "No Pixel" or "Pixelate" layer masks, just copy the source texture to the target
-                Blit(commandBuffer, sourceTexture, sourceTexture);
-            }
+            // Apply the material to the source texture and blit it to the target
+            Blit(commandBuffer, sourceTexture, tempTexture, material);
+            Blit(commandBuffer, tempTexture, sourceTexture);
 
             context.ExecuteCommandBuffer(commandBuffer);
             CommandBufferPool.Release(commandBuffer);
@@ -77,11 +59,9 @@ public class FullScreenRenderPassFeature : ScriptableRendererFeature
 
     public override void Create()
     {
-        LayerMask noPixelLayerMask = ~(1 << LayerMask.NameToLayer("No Pixel"));
-        LayerMask pixelateLayerMask = 1 << LayerMask.NameToLayer("Pixelate");
-        m_ScriptablePass = new CustomRenderPass(this.material, noPixelLayerMask, pixelateLayerMask);
+        m_ScriptablePass = new CustomRenderPass(this.material);
 
-// Before render
+        // Before render
         m_ScriptablePass.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
     }
 
