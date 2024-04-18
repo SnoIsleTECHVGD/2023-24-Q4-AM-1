7860dea80a96e8aa5eaa92c7ad0e1545d419601d
MUKILTEO\1604355 <1604355@mukilteo.wednet.edu>
2024-04-18 10:21:41 -0700

    Enemy Player Detection

--- NeonVoid/Assets/Ty/Code/EnemyTurn.cs
+++ NeonVoid/Assets/Ty/Code/EnemyTurn.cs
@@ -10,31 +10,50 @@ public class EnemyTurn : MonoBehaviour
     public Vector3[] ToPointPosition;
     public float MaxDistance;
     public int Loading;
-    
+    public Transform Enemy;
+    public LayerMask Player;
+    public RaycastHit hit;
+    public Ray ray;
+
+    Vector3 fwd;
+
+    private void Start()
+    {
+        fwd = Raycastobject.transform.TransformDirection(ToPointPosition[Loading]);
+        TriggerRaycast();
+    }
+
     public void TriggerRaycast()
     {
         Loading = 0;
         foreach(Transform T in ToPoints)
         {
-            Vector3 fwd = Raycastobject.transform.TransformDirection(ToPointPosition[Loading]);
-            Loading++;
-            Physics.Raycast(Raycastobject.transform.position, fwd);
-            
+            ray = new Ray(Enemy.position, ToPointPosition[Loading] - Enemy.position);
+
+            Debug.Log(Loading);
+            Physics.Raycast(ray);
+            if (Physics.Raycast(ray, out hit, Player))
+            {
+                Debug.Log("Player Found");
+            }
+            else
+            {
+                Loading++;
+            }
         }
-        
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
 
-        Vector3 direction = transform.TransformDirection(Vector3.forward) * 5;
+        Vector3 direction = Enemy.transform.TransformDirection(fwd) * 5;
 
-        Gizmos.DrawRay(transform.position, direction);
+        Gizmos.DrawRay(Raycastobject.transform.position, fwd);
     }
-
+   
     private void Update()
     {
-        
+
         
     }
 }
