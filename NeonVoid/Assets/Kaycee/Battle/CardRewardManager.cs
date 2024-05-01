68fc66a1e561c6dd4bce225eeb12b929b9e3367e
MUKILTEO\1713357 <Kolbscash@gmail.com>
2024-05-01 10:10:14 -0700

    most stuff done requires battle setup!!

---
+++ NeonVoid/Assets/Kaycee/Battle/CardRewardManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardRewardManager : MonoBehaviour
+{
+    GameManager gameManager;
+    public List<CardRewards> RewardsHolder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+    public void RandomizedCards()
+    {
+        gameManager.cardLibrary.Shuffle();
+        for (int i = 0;i < 3;i++)
+        {
+            RewardsHolder[i].gameObject.SetActive(true);
+            RewardsHolder[1].displayThree(gameManager.cardLibrary[i]);
+        }
+    }
+    public void SelectedCard(int cardIndex)
+    {
+        gameManager.playerDeck.Add(gameManager.cardLibrary[cardIndex]);
+        gameManager.cardLibrary.Remove(gameManager.cardLibrary[cardIndex]);
+
+        for (int i = 0; i < 3; i++)
+            RewardsHolder[i].gameObject.SetActive(false);
+
+    }
+}
--- NeonVoid/Assets/Kaycee/Battle/CardRewards.cs
+++ NeonVoid/Assets/Kaycee/Battle/CardRewards.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,9 +8,11 @@ public class CardRewards : MonoBehaviour
 {
     GameManager gameManager;
     public Image cardRewardIMG;
+    public TMP_Text cardDescription;
    public void displayThree(CardCode c)
     {
         cardRewardIMG.sprite = c.CardIcon;
+        cardDescription.text = c.cardDescription;
         
     }
 }
