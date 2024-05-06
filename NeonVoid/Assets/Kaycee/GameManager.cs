2efe0c81afcbbeaf4746ce4cb7ffa74726451d0f
MUKILTEO\1713357 <Kolbscash@gmail.com>
2024-05-06 09:54:16 -0700

    dont destroy on load gamemanger

--- NeonVoid/Assets/Kaycee/GameManager.cs
+++ NeonVoid/Assets/Kaycee/GameManager.cs
@@ -7,5 +7,9 @@ public class GameManager : MonoBehaviour
     public List<CardCode> playerDeck = new List<CardCode>();
     public List<CardCode> cardLibrary = new List<CardCode>();
     public List<CardCode> cardRewards = new List<CardCode>();
+    public void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
 
 }
